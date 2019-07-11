a3828e95e28827daa095415b932add2641d21c54
dahabu saidi <dahabusaidi@gmail.com>
2019-07-11 11:39:30 +0300

    add database connection

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 4007;
+const mongoose = require('mongoose');
+const config = require('./DB.js');
 const cors = require('cors');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
