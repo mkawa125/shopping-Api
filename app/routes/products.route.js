77373eefecd9ea017b11ed4c8f7f8500bbf50f8e
dahabu saidi <dahabusaidi@gmail.com>
2019-07-11 15:47:25 +0300

    add products controller

--- app/routes/products.route.js
+++ app/routes/products.route.js
@@ -1,16 +1,13 @@
 const Router = require('express');
 const router = new Router();
+const ProductsCOntroller = require('app/Controllers/ProductsController');
 
 
-// Require Post model in our routes module
-// let Post = require('/app/Models/products.model');
 
-// Defined get data(index or listing) route
 router.route('/').get(function (req, res) {
     res.status(200).json({'business': 'business is added successfully'});
 });
 
-// Defined store route
 router.route('/add').post(function (req, res) {
 
 });
