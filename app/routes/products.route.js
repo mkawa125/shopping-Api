877cc966b56261b27117de540f805fad315da4aa
dahabu saidi <dahabusaidi@gmail.com>
2019-07-11 22:14:14 +0300

    add delete products route

--- app/routes/products.route.js
+++ app/routes/products.route.js
@@ -5,5 +5,7 @@ const ProductsController = require('../Controllers/ProductsController');
 //Product routes
 router.route('/').get(ProductsController.getProducts);
 router.route('/add').post(ProductsController.addProduct);
+router.route('/delete/:id').delete(ProductsController.deleteProduct);
+router.route('/update/:id').post(ProductsController.updateProduct);
 
 module.exports=router;
