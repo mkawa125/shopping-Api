9bbedcc09659698eaaca4a51c202568ba934d19d
dahabu saidi <dahabusaidi@gmail.com>
2019-07-11 22:14:47 +0300

    add delete function and beautify response

--- app/Controllers/ProductsController.js
+++ app/Controllers/ProductsController.js
@@ -7,7 +7,13 @@ module.exports = {
                 res.json(err);
             }
             else {
-                res.json(products);
+                res.json(
+                    {
+                        "success": true,
+                        "message": "Products retrieved successfully",
+                        "products": products
+                    },
+                );
             }
         })
     },
@@ -21,5 +27,22 @@ module.exports = {
             .catch(() => {
                 res.status(400).send("unable to save to add product");
             });
+    },
+
+    // Update Selected Product
+    updateProduct: async (req, res) => {
+        res.json('Am edit this product')
+    },
+
+    // Remove | Delete product from database
+    deleteProduct: async (req, res) => {
+        product.findByIdAndRemove({_id: req.params.id}, function(err){
+            if(err) {
+                res.status(400).json(err);
+            }
+            else {
+                res.status(200).json('Product Successfully Delete');
+            }
+        });
     }
 };
